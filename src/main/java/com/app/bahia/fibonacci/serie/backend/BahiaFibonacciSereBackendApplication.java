31b0833732b16bc6b51598217714fa524c2acb4c
Cristian Moreno <cristian.moreno@avoristravel.com>
2020-08-29 10:48:01 +0200

    #master
    Initial commit

---
+++ src/main/java/com/app/bahia/fibonacci/serie/backend/BahiaFibonacciSereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.bahia.fibonacci.serie.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahiaFibonacciSereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BahiaFibonacciSereBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/bahia/fibonacci/serie/backend/BahiaFibonacciSereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bahia.fibonacci.serie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahiaFibonacciSereBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
